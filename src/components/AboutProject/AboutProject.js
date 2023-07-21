e5fc01f29c65c4613e5e1ab89b810e537c98ea8e
IraGrafova <i.grafova@ntc-hitech.ru>
2023-07-21 23:43:14 +0300

    do about-project

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,17 +1,39 @@
 import React from "react";
 
-import './AboutProject.css';
+import "./AboutProject.css";
 
 function AboutProject() {
-    return (
-        <section className="about-project">
-            <h2 className="about-project__title">О проекте</h2> 
-            <h3 className="about-project__title">Дипломный проект включал 5 этапов</h3>
-            <p className="about-project__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-            <h3 className="about-project__title">На выполнение диплома ушло 5 недель</h3>
-            <p className="about-project__description">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-        </section>
-    );
+  return (
+    <section className="about-project">
+      <h2 className="about-project__title">О проекте</h2>
+      <div className="about-project__blocks">
+      <div className="about-project__block">
+        <h3 className="about-project__title-description">
+          Дипломный проект включал 5 этапов
+        </h3>
+        <p className="about-project__description">
+          Составление плана, работу над бэкендом, вёрстку, добавление
+          функциональности и финальные доработки.
+        </p>
+        <div className="about-project__block">
+        <h3 className="about-project__title-description">
+          На выполнение диплома ушло 5 недель
+        </h3>
+        <p className="about-project__description">
+          У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+          соблюдать, чтобы успешно защититься.
+        </p>
+        </div>
+      </div>
+      </div>
+      <div className="about-project__grid">
+        <h4 className="about-project__grid-title">1 неделя</h4>
+        <h4 className="about-project__grid-title about-project__grid-title_long">4 недели</h4>
+        <p className="about-project__grid-description">Back-end</p>
+        <p className="about-project__grid-description">Front-end</p>
+      </div>
+    </section>
+  );
 }
 
 export default AboutProject;

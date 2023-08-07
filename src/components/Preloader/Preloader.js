298f1e057c80cfe071735ceebccf1d760e6bedde
IraGrafova <i.grafova@ntc-hitech.ru>
2023-08-07 13:27:08 +0300

    do preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react'
+import './Preloader.css'
 
-import "./Preloader.css";
+const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div className="preloader__container">
+                <span className="preloader__round"></span>
+            </div>
+        </div>
+    )
+};
 
-function Preloader() {
-  return (
-    <section className="preloader">
-
-    </section>
-  );
-}
-
-export default Preloader;
+export default Preloader

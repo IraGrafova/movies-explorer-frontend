db67bbac689c0c91c4b7662cbd69e7406c78b7a7
IraGrafova <i.grafova@ntc-hitech.ru>
2023-08-26 17:08:32 +0300

    base addr

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://api.grafs-diplom.nomoredomains.xyz";
 
 export const register = (data) => {
   return fetch(`${baseUrl}/signup`, {
